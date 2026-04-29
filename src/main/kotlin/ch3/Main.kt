06cdd86c66f2159fd10ec6b11aa6fa3a0383ed0a
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-04-29 09:37:23 +0900

    val, var

---
+++ src/main/kotlin/ch3/Main.kt
@@ -0,0 +1,12 @@
+package com.back.ch3
+
+fun main() {
+    val number = 10
+    var message = "Hello, Java!"
+
+    message += "Kotlin"
+
+    println("Number: ${number}")
+    println("Message: ${message}")
+
+}
