f2cf98adaec3ca6c41097796a09ec8f1eba9e1d2
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-04-29 09:12:54 +0900

    start

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.3.0"
+}
+
+group = "com.back"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
